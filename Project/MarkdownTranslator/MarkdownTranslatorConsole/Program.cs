eff2f23528efbadf6eb990f547e0e5752c012654
TanakaDorei <43180313+TANAKADOREI@users.noreply.github.com>
2022-09-26 11:33:26 +0900

    make project

---
+++ Project/MarkdownTranslator/MarkdownTranslatorConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using MarkdownTranslatorCore;
+using System;
+using System.IO;
+
+namespace MarkdownTranslatorConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            args = new string[] { "README.md", "README_EN.md" , "README_JP.md" };
+            string result_en;
+            string result_jp;
+            string error_msg = MarkdownTranslator.Run(File.ReadAllText(args[0]), out result_en,out result_jp);
+            if (error_msg != null)
+                Console.WriteLine(error_msg);
+            else
+            {
+                File.WriteAllText(args[1], result_en);
+                File.WriteAllText(args[2], result_jp);
+                Console.WriteLine("Complete");
+            }
+        }
+    }
+}
---
+++ Project/MarkdownTranslator/MarkdownTranslatorCore/Class1.cs
@@ -0,0 +1,54 @@
+﻿using GoogleTranslateFreeApi;
+using Markdig;
+using Markdig.Syntax;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkdownTranslatorCore
+{
+    public static class MarkdownTranslator
+    {
+        public static string Run(string source, out string _dest_en,out string _dest_jp)
+        {
+            string dest_en = "";
+            string dest_jp = "";
+
+            var task = Task.Run(async () =>
+            {
+                var md = Markdown.Parse(source);
+                var translator = new GoogleTranslator();
+
+                //Language from = Language.Auto;
+                Language from = Language.Korean;
+                Language to_en = Language.English;
+                Language to_jp = Language.Japanese;
+
+
+                foreach (LeafBlock i in from i in md where i is LeafBlock select i)
+                {
+                    if (i.Inline == null) continue;
+                    var text = i.Inline.FirstChild.ToString();
+
+                    var result = await translator.TranslateLiteAsync(text, from, to_en);
+
+                    if (result.Corrections.TextWasCorrected)
+                        Console.WriteLine(string.Join(",", result.Corrections.CorrectedWords));
+
+                    Console.WriteLine(text);
+                    Console.WriteLine($"[{result.MergedTranslation}]");
+                }
+
+                return "d";
+            });
+
+            while(true) { }
+
+            _dest_en = dest_en;
+            _dest_jp = dest_jp;
+
+            return task.Result;
+        }
+    }
+}
